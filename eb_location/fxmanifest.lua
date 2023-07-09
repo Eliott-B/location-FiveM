a20fe26efbf9fe0dc9cf2aa1c0dc59573f35cb7e
Eliott-B <eliottb.info@gmail.com>
2023-07-09 17:06:39 +0200

    🚚 | Move files

--- eb_location/fxmanifest.lua
+++ eb_location/fxmanifest.lua
@@ -6,8 +6,8 @@ description 'Print ped location.'
 version '1.0.0'
 
 client_scripts {
-    "cl_location.lua",
-    "cl_locationonscreen.lua"
+    "clients/cl_location.lua",
+    "clients/cl_locationonscreen.lua"
 }
 
 shared_script "config.lua"
